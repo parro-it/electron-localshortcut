8c8de7acba2a0f3bfdee3bcd627715d1edbe2e4c
Andrea Parodi <andrea@parro.it>
2016-06-25 16:49:39 +0200

    fixed lint style for xo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const electron = require('electron');
+
 const globalShortcut = electron.globalShortcut;
 const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
@@ -11,139 +12,138 @@ const windowsWithShortcuts = new WeakMap();
 const ANY_WINDOW = {};
 
 function isAccelerator(arg) {
-  return typeof arg === 'string';
+	return typeof arg === 'string';
 }
 
 function unregisterAllShortcuts(win) {
-  const shortcuts = windowsWithShortcuts.get(win);
-  shortcuts.forEach( sc =>
-    globalShortcut.unregister(sc.accelerator)
-  );
+	const shortcuts = windowsWithShortcuts.get(win);
+	shortcuts.forEach(sc =>
+		globalShortcut.unregister(sc.accelerator)
+	);
 }
 
 function registerAllShortcuts(win) {
-  const shortcuts = windowsWithShortcuts.get(win);
+	const shortcuts = windowsWithShortcuts.get(win);
 
-  shortcuts.forEach( sc =>
-    globalShortcut.register(sc.accelerator, sc.callback)
-  );
+	shortcuts.forEach(sc =>
+		globalShortcut.register(sc.accelerator, sc.callback)
+	);
 }
 
 function unregisterAll(win) {
-  if (win === undefined) {
-    // unregister shortcuts for any window in the app
-    unregisterAll(ANY_WINDOW);
-    return;
-  }
+	if (win === undefined) {
+		// unregister shortcuts for any window in the app
+		unregisterAll(ANY_WINDOW);
+		return;
+	}
 
-  if (!windowsWithShortcuts.has(win)) {
-    return;
-  }
+	if (!windowsWithShortcuts.has(win)) {
+		return;
+	}
 
-  unregisterAllShortcuts(win);
-  windowsWithShortcuts.delete(win);
+	unregisterAllShortcuts(win);
+	windowsWithShortcuts.delete(win);
 }
 
 function register(win, accelerator, callback) {
-  if (arguments.length === 2 && isAccelerator(win)) {
-    // register shortcut for any window in the app
-    // win = accelerator, accelerator = callback
-    register(ANY_WINDOW, win, accelerator);
-    return;
-  }
+	if (arguments.length === 2 && isAccelerator(win)) {
+		// register shortcut for any window in the app
+		// win = accelerator, accelerator = callback
+		register(ANY_WINDOW, win, accelerator);
+		return;
+	}
 
-  if (windowsWithShortcuts.has(win)) {
-    const shortcuts = windowsWithShortcuts.get(win);
-    shortcuts.push({
-      accelerator: accelerator,
-      callback: callback
-    });
-  } else {
-    windowsWithShortcuts.set(win, [{
-      accelerator: accelerator,
-      callback: callback
-    }]);
-  }
+	if (windowsWithShortcuts.has(win)) {
+		const shortcuts = windowsWithShortcuts.get(win);
+		shortcuts.push({
+			accelerator,
+			callback
+		});
+	} else {
+		windowsWithShortcuts.set(win, [{
+			accelerator,
+			callback
+		}]);
+	}
 
-  const focusedWin = BrowserWindow.getFocusedWindow();
-  if ((win === ANY_WINDOW && focusedWin !== null) || focusedWin === win) {
-    globalShortcut.register(accelerator, callback);
-  }
+	const focusedWin = BrowserWindow.getFocusedWindow();
+	if ((win === ANY_WINDOW && focusedWin !== null) || focusedWin === win) {
+		globalShortcut.register(accelerator, callback);
+	}
 }
 
 function indexOfShortcut(win, accelerator) {
-  if (!windowsWithShortcuts.has(win)) {
-    return -1;
-  }
+	if (!windowsWithShortcuts.has(win)) {
+		return -1;
+	}
 
-  const shortcuts = windowsWithShortcuts.get(win);
-  let shortcutToUnregisterIdx = -1;
-  shortcuts.some((s, idx) => {
-    if (s.accelerator === accelerator) {
-      shortcutToUnregisterIdx = idx;
-      return true;
-    }
-    return false;
-  });
-  return shortcutToUnregisterIdx;
+	const shortcuts = windowsWithShortcuts.get(win);
+	let shortcutToUnregisterIdx = -1;
+	shortcuts.some((s, idx) => {
+		if (s.accelerator === accelerator) {
+			shortcutToUnregisterIdx = idx;
+			return true;
+		}
+		return false;
+	});
+	return shortcutToUnregisterIdx;
 }
 
 function unregister(win, accelerator) {
-  if (arguments.length === 1 && isAccelerator(win)) {
-    // unregister shortcut for any window in the app
-    // win = accelerator
-    unregister(ANY_WINDOW, win);
-    return;
-  }
-  const shortcutToUnregisterIdx = indexOfShortcut(win, accelerator);
+	if (arguments.length === 1 && isAccelerator(win)) {
+		// unregister shortcut for any window in the app
+		// win = accelerator
+		unregister(ANY_WINDOW, win);
+		return;
+	}
+	const shortcutToUnregisterIdx = indexOfShortcut(win, accelerator);
 
-  if (shortcutToUnregisterIdx !== -1) {
-    globalShortcut.unregister(accelerator);
-    const shortcuts = windowsWithShortcuts.get(win);
-    shortcuts.splice(shortcutToUnregisterIdx, 1);
-  }
+	if (shortcutToUnregisterIdx !== -1) {
+		globalShortcut.unregister(accelerator);
+		const shortcuts = windowsWithShortcuts.get(win);
+		shortcuts.splice(shortcutToUnregisterIdx, 1);
+	}
 }
 
 function isRegistered(win, accelerator) {
-  if (arguments.length === 1 && isAccelerator(win)) {
-    // check shortcut for any window in the app
-    // win = accelerator
-    return isRegistered(ANY_WINDOW, win);
-  }
+	if (arguments.length === 1 && isAccelerator(win)) {
+		// check shortcut for any window in the app
+		// win = accelerator
+		return isRegistered(ANY_WINDOW, win);
+	}
 
-  return indexOfShortcut(win, accelerator) !== -1;
+	return indexOfShortcut(win, accelerator) !== -1;
 }
 
-
 app.on('browser-window-focus', (e, win) => {
-  if (windowsWithShortcuts.has(ANY_WINDOW)) {
-    registerAllShortcuts(ANY_WINDOW);
-  }
+	if (windowsWithShortcuts.has(ANY_WINDOW)) {
+		registerAllShortcuts(ANY_WINDOW);
+	}
 
-  if (!windowsWithShortcuts.has(win)) {
-    return;
-  }
+	if (!windowsWithShortcuts.has(win)) {
+		return;
+	}
 
-  registerAllShortcuts(win);
+	registerAllShortcuts(win);
 });
 
 app.on('browser-window-blur', (e, win) => {
-  if (windowsWithShortcuts.has(ANY_WINDOW)) {
-    unregisterAllShortcuts(ANY_WINDOW);
-  }
+	if (windowsWithShortcuts.has(ANY_WINDOW)) {
+		unregisterAllShortcuts(ANY_WINDOW);
+	}
 
-  if (!windowsWithShortcuts.has(win)) {
-    return;
-  }
+	if (!windowsWithShortcuts.has(win)) {
+		return;
+	}
 
-  unregisterAllShortcuts(win);
+	unregisterAllShortcuts(win);
 });
 
 module.exports = {
-  register: register,
-  unregister: unregister,
-  isRegistered: isRegistered,
-  unregisterAll: unregisterAll,
-  enableAll: registerAllShortcuts,
-  disableAll: unregisterAllShortcuts
+	register,
+	unregister,
+	isRegistered,
+	unregisterAll,
+	enableAll: registerAllShortcuts,
+	disableAll: unregisterAllShortcuts
 };
--- test.js
+++ test.js
@@ -1,116 +1,113 @@
 'use strict';
 
-const electronLocalshortcut = require('.');
 const electron = require('electron');
+const electronLocalshortcut = require('.');
+
 const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
 const Menu = electron.Menu;
 
-
 app.on('ready', () => {
-  const win = new BrowserWindow({});
-  const win2 = new BrowserWindow({});
+	const win = new BrowserWindow({});
+	const win2 = new BrowserWindow({});
 
-  electronLocalshortcut.register(win, 'Ctrl+A', () => {
-    process.stdout.write('A\n');
-  });
+	electronLocalshortcut.register(win, 'Ctrl+A', () => {
+		process.stdout.write('A\n');
+	});
 
-  electronLocalshortcut.register(win, 'Ctrl+B', () => {
-    process.stdout.write('B\n');
-  });
+	electronLocalshortcut.register(win, 'Ctrl+B', () => {
+		process.stdout.write('B\n');
+	});
 
+	electronLocalshortcut.register(win2, 'Ctrl+A', () => {
+		process.stdout.write('A2\n');
+	});
 
-  electronLocalshortcut.register(win2, 'Ctrl+A', () => {
-    process.stdout.write('A2\n');
-  });
+	electronLocalshortcut.register(win2, 'Ctrl+B', () => {
+		process.stdout.write('B2\n');
+	});
 
-  electronLocalshortcut.register(win2, 'Ctrl+B', () => {
-    process.stdout.write('B2\n');
-  });
+	electronLocalshortcut.register(win2, 'Ctrl+C', () => {
+		process.stdout.write('C2\n');
+	});
 
-  electronLocalshortcut.register(win2, 'Ctrl+C', () => {
-    process.stdout.write('C2\n');
-  });
+	const template = [{
+		label: 'test',
+		submenu: [
+			{
+				label: 'Unregister all shortcuts',
+				click() {
+					electronLocalshortcut.unregisterAll(win);
+				}
+			}
+		]
+	}];
 
-  const template = [{
-    label: 'test',
-    submenu: [
-      {
-        label: 'Unregister all shortcuts',
-        click() {
-          electronLocalshortcut.unregisterAll(win);
-        }
-      }
-    ]
-  }];
+	win.setMenu(Menu.buildFromTemplate(template));
+	win.loadURL('about://blank');
+	win.show();
 
-  win.setMenu(Menu.buildFromTemplate(template));
-  win.loadURL('about://blank');
-  win.show();
+	const template2 = [{
+		label: 'test',
+		submenu: [{
+			label: 'Unregister C2',
+			click() {
+				electronLocalshortcut.unregister(win2, 'Ctrl+C');
+			}
+		}, {
+			label: 'Register C2',
+			click() {
+				electronLocalshortcut.register(win2, 'Ctrl+C', () => {
+					process.stdout.write('C2\n');
+				});
+			}
+		}, {
+			label: 'Disable shortcuts',
+			click() {
+				electronLocalshortcut.disableAll(win2);
+			}
+		}, {
+			label: 'Enable shortcuts',
+			click() {
+				electronLocalshortcut.enableAll(win2);
+			}
+		}, {
+			label: 'Check C2',
+			click() {
+				const isRegistered = electronLocalshortcut.isRegistered(win2, 'Ctrl+C');
+				process.stdout.write(`${isRegistered}\n`);
+			}
+		}]
+	}, {
+		label: 'test all window',
+		submenu: [{
+			label: 'Unregister ALL',
+			click() {
+				electronLocalshortcut.unregister('Alt+A');
+			}
+		}, {
+			label: 'Unregister any ALL',
+			click() {
+				electronLocalshortcut.unregisterAll();
+			}
+		}, {
+			label: 'Register ALL',
+			click() {
+				electronLocalshortcut.register('Alt+A', () => {
+					process.stdout.write('ALL\n');
+				});
+			}
+		}, {
+			label: 'Check ALL',
+			click() {
+				const isRegistered = electronLocalshortcut.isRegistered('Alt+A');
+				process.stdout.write(`${isRegistered}\n`);
+			}
+		}]
+	}];
 
-  const template2 = [{
-    label: 'test',
-    submenu: [{
-      label: 'Unregister C2',
-      click() {
-        electronLocalshortcut.unregister(win2, 'Ctrl+C');
-      }
-    }, {
-      label: 'Register C2',
-      click() {
-        electronLocalshortcut.register(win2, 'Ctrl+C', () => {
-          process.stdout.write('C2\n');
-        });
-      }
-    }, {
-      label: 'Disable shortcuts',
-      click() {
-        electronLocalshortcut.disableAll(win2);
-      }
-    }, {
-      label: 'Enable shortcuts',
-      click() {
-        electronLocalshortcut.enableAll(win2);
-      }
-    }, {
-      label: 'Check C2',
-      click() {
-        process.stdout.write(
-          electronLocalshortcut.isRegistered(win2, 'Ctrl+C') + '\n'
-        );
-      }
-    }]
-  }, {
-    label: 'test all window',
-    submenu: [{
-      label: 'Unregister ALL',
-      click() {
-        electronLocalshortcut.unregister('Alt+A');
-      }
-    }, {
-      label: 'Unregister any ALL',
-      click() {
-        electronLocalshortcut.unregisterAll();
-      }
-    }, {
-      label: 'Register ALL',
-      click() {
-        electronLocalshortcut.register('Alt+A', () => {
-          process.stdout.write('ALL\n');
-        });
-      }
-    }, {
-      label: 'Check ALL',
-      click() {
-        process.stdout.write(
-          electronLocalshortcut.isRegistered('Alt+A') + '\n'
-        );
-      }
-    }]
-  }];
-
-  win2.setMenu(Menu.buildFromTemplate(template2));
-  win2.loadURL('about://blank');
-  win2.show();
+	win2.setMenu(Menu.buildFromTemplate(template2));
+	win2.loadURL('about://blank');
+	win2.show();
 });
 
