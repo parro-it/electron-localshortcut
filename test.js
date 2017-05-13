423af1e6b5892c4dd928bacb82fa83ca567013dd
Andrea Parodi <andrea@parro.it>
2017-05-13 11:30:59 +0200

    other tests

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 'use strict';
 const {BrowserWindow, app} = require('electron');
 const test = require('tape-async');
-const pEvent = require('p-event');
-const delay = require('delay');
 const pTimeout = require('p-timeout');
+const {appReady, focusWindow, minimizeWindow} = require('p-electron');
 
 const shortcuts = require('.');
 
@@ -33,74 +32,70 @@ function createMock() {
 	return {keypress};
 }
 
-function appReady() {
-	if (app.isReady()) {
-		return Promise.resolve();
-	}
-	return pEvent(app, 'ready');
-}
-
-test('exports an appReady function', async t => {
-	t.is(typeof shortcuts, 'object');
-});
-
-const beforeAll = async () => {
+async function beforeAll() {
 	await appReady();
 	// We could create a window, because the app is ready
 	win = new BrowserWindow();
 	win.loadURL('https://example.com');
 	win.show();
-};
+}
+
+async function shortcutIsNotEnabledOnRegistering(key, win) {
+	const callbackCalled = new Promise(resolve => shortcuts.register(win, key, resolve));
+	mock.keypress(key);
+	const err = await pTimeout(callbackCalled, 400).catch(err => err);
+	return err instanceof Error && err.message === 'Promise timed out after 400 milliseconds';
+}
+
+async function shortcutIsEnabledOnRegistering(key, win) {
+	const callbackCalled = new Promise(resolve => shortcuts.register(win, key, resolve));
+	mock.keypress(key);
+	return undefined === await pTimeout(callbackCalled, 100);
+}
+
+test('exports an shortcuts object', async t => {
+	t.is(typeof shortcuts, 'object');
+});
+
+test('appReady return a promise that resolve when electron app is ready', beforeAll);
 
 test('shortcut is enabled on registering if window is focused', async t => {
 	const win2 = new BrowserWindow();
-	win2.focus();
-	await delay(100);
-
-	const callbackCalled = new Promise(resolve => shortcuts.register(win2, 'Ctrl+A', resolve));
-	mock.keypress('Ctrl+A');
-	t.is(await pTimeout(callbackCalled, 100), undefined);
+	await focusWindow(win2);
+	t.true(shortcutIsEnabledOnRegistering('Ctrl+A', win2));
 	win2.close();
 });
 
 test('shortcut is not enabled on registering if window is not focused', async t => {
 	const win2 = new BrowserWindow();
-	const callbackCalled = new Promise(resolve => shortcuts.register(win2, 'Ctrl+B', resolve));
-	mock.keypress('Ctrl+B');
-	const err = await pTimeout(callbackCalled, 400).catch(err => err);
-	t.is(err.message, 'Promise timed out after 400 milliseconds');
+	t.true(await shortcutIsNotEnabledOnRegistering('Ctrl+B', win2));
 	win2.close();
 });
-
-test('appReady return a promise that resolve when electron app is ready', beforeAll);
 
 test('shortcut is not enabled on registering if window is minimized', async t => {
 	const win2 = new BrowserWindow();
-	win2.focus();
-	await pEvent(win2, 'focus');
-	t.true(win2.isFocused());
+	await focusWindow(win2);
+	await minimizeWindow(win2);
 
-	win2.minimize();
-	await pEvent(win2, 'minimize');
 	t.true(win2.isMinimized());
 
-	t.true(await shortcutIsNotEnabledOnRegistering('Ctrl+F', win2));
+	t.true(await shortcutIsNotEnabledOnRegistering('Ctrl+W', win2));
 
 	win2.close();
 });
 
-async function shortcutIsNotEnabledOnRegistering(key, win) {
-	const callbackCalled = new Promise(resolve => shortcuts.register(win, key, resolve));
-	mock.keypress(key);
-	const err = await pTimeout(callbackCalled, 100).catch(err => err);
-	return err instanceof Error && err.message === 'Promise timed out after 100 milliseconds';
-}
-
 test('shortcut is not enabled on registering if window is not showed', async t => {
 	const win2 = new BrowserWindow({show: false});
-
 	t.true(await shortcutIsNotEnabledOnRegistering('Ctrl+F', win2));
+	win2.close();
+});
 
+test('shortcut is not enabled on registering if window is hidden', async t => {
+	const win2 = new BrowserWindow();
+	t.true(win2.isVisible());
+	win2.hide();
+	t.false(win2.isVisible());
+	t.true(await shortcutIsNotEnabledOnRegistering('Ctrl+F', win2));
 	win2.close();
 });
 
